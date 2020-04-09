dfc2246b26344d53fe2d614420b7946f9d08a1d5
noahstrac <nstracqu@uvm.edu>
2020-04-09 13:03:47 -0400

    fixed spaces not working and added null checking

--- graph.py
+++ graph.py
@@ -13,21 +13,37 @@ varY = sys.argv[4]
 data = open(filename, 'r')
 data = csv.reader(data)
 
+# Initialize variables
 x = []
 y = []
+null = 0
 
+# Add data to variables
 for obs in data:
-    x.append(obs[0])
-    y.append(obs[1])
+    try:
+        # make sure both variables are ints/floats
+        float(obs[0])
+        float(obs[1])
+        # if so, add them to the lists
+        x.append(float(obs[0]))
+        y.append(float(obs[1]))
+    except:
+        # otherwise add a null value without appending anything to the lists
+        null += 1
 
 # Plot the data
 plt.plot(x,y, 'o')
 plt.xlabel(varX)
 plt.ylabel(varY)
 plt.title(title)
+
+# Save and open figure
 plt.savefig("graph.pdf")
 if sys.platform == "win32":
     os.system("explorer.exe graph.pdf")
 else:
     os.system("open graph.pdf")
-#print("File created: graph.pdf at location {}".format(os.getcwd()))
+
+# Display some helpful output
+print("File created: graph.pdf at location {}".format(os.getcwd()))
+print("There were " + str(null) + " null, titles, or incorrectly formatted values in the dataset.")
--- main.cpp
+++ main.cpp
@@ -14,13 +14,11 @@ const string python = "python3";
 string getResponse(string prompt);
 
 int main(){
-    cout << "Welcome to our data graphing service! Enter your responses to the questions below to receive the graph output of your .csv data set." << endl;
+    cout << "Welcome to our scatter plotting service! Enter your responses to the questions below to receive the graph output of your .csv data set." << endl;
     string title = getResponse("Title of graph: ");
     string varX = getResponse("Label for x variable: ");
     string varY = getResponse("Label for y variable: ");
     string dataFile = getResponse("Enter the .csv file where your data is held (ex. myfile.csv): ");
-    // TODO: we can add a file reading section here that either confirms that each of the values in the csv is a number, or just casts/forces each entry to be a number
-    // TODO: regardless of what it is in the first place.
 
     string command = python + " graph.py " + dataFile + " " + title + " " + varX + " " + varY;
     system(command.c_str());
@@ -32,5 +30,6 @@ string getResponse(string prompt){
     string response;
     cout << prompt;
     getline(cin, response);
+    response = '"' + response + '"';
     return response;
 }
